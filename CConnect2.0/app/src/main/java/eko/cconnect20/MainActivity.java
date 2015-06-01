d77bdc88d592323d6ebbf3ec89e4c73055c1a53e
Bivesh <bivs06@gmail.com>
2015-06-01 16:40:39 +0530

    month list changed

--- CConnect2.0/app/src/main/java/eko/cconnect20/MainActivity.java
+++ CConnect2.0/app/src/main/java/eko/cconnect20/MainActivity.java
@@ -15,7 +15,7 @@ import java.util.Calendar;
 public class MainActivity extends ActionBarActivity {
 
 
-    private String[] month= {"Month","Jan(1)","Feb(2)","Mar(3)","Apr(4)","May(5)","Jun(6)", "Jul(7)", "Aug(8)","Sep(9)", "Oct(10)","Nov(11)","Dec(12)"};
+    private String[] month= {"Month","Jan","Feb","Mar","Apr","May","Jun", "Jul", "Aug","Sep", "Oct","Nov","Dec"};
 
     private String year[] =  new String[100];
 
